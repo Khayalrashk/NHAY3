e21215b3dcb4c5f73a225ccdebbc14a55dabcdae
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-08-26 01:18:10 +0300

    Update and rename database (2).py to database.py

--- ZeMusic/utils/database.py
+++ ZeMusic/utils/database.py
@@ -1,8 +1,8 @@
 import random
 from typing import Dict, List, Union
 
-from AnonXMusic import userbot
-from AnonXMusic.core.mongo import mongodb
+from ZeMusic import userbot
+from ZeMusic.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
